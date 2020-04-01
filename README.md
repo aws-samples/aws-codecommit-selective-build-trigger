d324546f89c1ca2927860edaae7003f21bfeebaa
Praveen Kumar Jeyarajan <pjeyaraj@amazon.com>
2020-04-01 16:35:42 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,13 +1,44 @@
-## My Project
+# aws-codecommit-selective-build-trigger
 
-TODO: Fill this README out!
+This demo illustrates the deployment of AWS Lambda to build Docker Images of application in AWS CodeCommit repository automatically when selective files are modified and the changes are pushed to the repository. The AWS Lambda receives the AWS CodeCommit events for push to repository and triggers a AWS CodeBuild job to build the Docker image and push to AWS Elastic Container Registry.
 
-Be sure to:
+AWS services used for the CI/CD portion:
 
-* Change the title in this README
-* Edit your repository description on GitHub
+- [AWS CodeCommit](https://aws.amazon.com/codecommit/)
+- [AWS CodeBuild](https://aws.amazon.com/codebuild/)
+- [AWS CloudFormation](https://aws.amazon.com/cloudformation/)
+- [AWS Lambda](https://aws.amazon.com/lambda/)
+- [Amazon Elastic Container Registry](https://aws.amazon.com/ecr/)
 
-## License
+## Solution Diagram
+
+![Solution Diagram](assets/aws-codecommit-selective-build-trigger.png)
+
+## Stack deployment
+
+The cloudformation stack can be deployed using Cloudoformation page in AWS Console or using the AWS CLI as shown below
+
+First, zip and upload the lambda code to an S3 bucket
+
+`cd src/`
+
+`zip lambda.zip lambda_code.py`
+
+`aws s3 cp lambda.zip s3://aws-codecommit-selective-build-trigger/`
+
+Trigger the cloudformation stack creation pointing to that S3 bucket zip.
+
+`aws cloudformation create-stack --stack-name myteststack --template-body file://src/aws-codecommit-selective-build-trigger.yml --parameters ParameterKey=ProjectName,ParameterValue=TestProject ParameterKey=LambdaZipS3Bucket,ParameterValue=datarepos2601 ParameterKey=LambdaZipS3Key,ParameterValue=lambda.zip`
+
+## Components details
+
+[src/aws-codecommit-selective-build-trigger.yml](src/aws-codecommit-selective-build-trigger.yml) - Cloudformation template for demonstrating the solution of AWS Lambda triggered AWS CodeBuild job based on changes to specific files in AWS CodeCommit repository
+
+[src/lambda_code.py](src/lambda_code.py) - Python code for AWS Lambda to filter the AWS CodeCommit event and find the files changed as part of the commit and trigger AWS CodeBuild job if needed.
+
+[src/lambda.zip](src/lambda.zip) - Compressed zip file for lambda_code.py file for deploying using Cloudformation template
+
+# License
 
 This library is licensed under the MIT-0 License. See the LICENSE file.
 
---
+++ src/lambda_code.py
@@ -0,0 +1,141 @@
+""" 
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+"""
+
+import boto3
+import os
+from botocore.exceptions import ClientError
+
+CODE_BUILD_PROJECT = os.getenv('CODE_BUILD_PROJECT')
+ECR_REPO_NAME = os.getenv('ECR_REPO_NAME')
+
+codecommit = boto3.client('codecommit')
+cb = boto3.client( 'codebuild' )
+       
+def getLastCommitLog(repository, commitId):
+    response = codecommit.get_commit(
+        repositoryName=repository,
+        commitId=commitId
+    )
+    return response['commit']
+
+def getFileDifferences(repository_name, lastCommitID, previousCommitID):
+    response = None
+
+    if previousCommitID != None:
+        response = codecommit.get_differences(
+            repositoryName=repository_name,
+            beforeCommitSpecifier=previousCommitID,
+            afterCommitSpecifier=lastCommitID
+        )
+    else:
+        # The case of getting initial commit (Without beforeCommitSpecifier)
+        response = codecommit.get_differences(
+            repositoryName=repository_name,
+            afterCommitSpecifier=lastCommitID
+        )
+
+    differences = []
+
+    if response == None:
+        return differences
+
+    while "nextToken" in response:
+        response = codecommit.get_differences(
+            repositoryName=repository_name,
+            beforeCommitSpecifier=previousCommitID,
+            afterCommitSpecifier=lastCommitID,
+            nextToken=response["nextToken"]
+        )
+        differences += response.get("differences", [])
+    else:
+        differences += response["differences"]
+
+    return differences
+    
+def getLastCommitID(repository, branch="master"):
+    response = codecommit.get_branch(
+        repositoryName=repository,
+        branchName=branch
+    )
+    commitId = response['branch']['commitId']
+    return commitId
+    
+def lambda_handler( event, context ):
+    ######################### REQUIRED VARS ###################################
+    file_extension_allowed = [".pyo", ".npy", ".py"]
+    fileNames_allowed = ["DockerFile", "Dockerfile"]
+    commit_hash = event['Records'][0]['codecommit']['references'][0]['commit']
+    region = event['Records'][0]['awsRegion']
+    repo_name = event['Records'][0]['eventSourceARN'].split(':')[-1]
+    account_id = event['Records'][0]['eventSourceARN'].split(':')[4]
+    branchName = os.path.basename(str(event['Records'][0]['codecommit']['references'][0]['ref']))
+    
+    if (commit_hash == None) or (commit_hash == '0000000000000000000000000000000000000000'):
+        commit_hash = getLastCommitID(repo_name, branchName)
+    
+    lastCommit = getLastCommitLog(repo_name, commit_hash)
+    
+    previousCommitID = None
+    if len(lastCommit['parents']) > 0:
+        previousCommitID = lastCommit['parents'][0]
+
+    print('lastCommitID: {0} previousCommitID: {1}'.format(commit_hash, previousCommitID))
+
+    differences = getFileDifferences(repo_name, commit_hash, previousCommitID)
+    print(differences)
+    
+    doTriggerBuild = False
+    for diff in differences:
+        root, extension = os.path.splitext(str(diff['afterBlob']['path']))
+        fileName = os.path.basename(str(diff['afterBlob']['path']))
+        if ((extension in file_extension_allowed) or (fileName in fileNames_allowed)):
+            doTriggerBuild = True
+    
+    if doTriggerBuild:
+        ###################### CODE BUILD #########################################
+        build = {
+            'projectName': CODE_BUILD_PROJECT,
+            'sourceVersion': commit_hash,
+            'sourceTypeOverride': 'CODECOMMIT',
+            'sourceLocationOverride': 'https://git-codecommit.%s.amazonaws.com/v1/repos/%s' % (region, repo_name),
+            'environmentVariablesOverride': [
+                {
+                    'name': 'AWS_DEFAULT_REGION',
+                    'value': region,
+                    'type': 'PLAINTEXT'
+                },
+                {
+                    'name': 'ECR_REPO',
+                    'value': ECR_REPO_NAME,
+                    'type': 'PLAINTEXT'
+                },
+                {
+                    'name': 'AWS_ACCOUNT_ID',
+                    'value': account_id,
+                    'type': 'PLAINTEXT'
+                }
+            ]
+        }
+        
+        print("building docker image from repo %s in region %s" % (repo_name, region))
+        
+        # build all the things and push to ecr!
+        cb.start_build( **build )
+    else:
+        print ('Changed files does not match any triggers. Hence docker image build is suppressed')
+    return 'Success.'
